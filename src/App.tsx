12af04e01f19a3e0257eb23c31f37add4f16bf79
Raitis Iziks <raitisiziks@gmail.com>
2024-11-24 15:44:12 +0200

    Implement ProductPage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import MainContent from "./components/MainContent";
+import ProductPage from "./components/ProductPage";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <div className="rounded w-full flex justify-between flex-wrap">
           <Routes>
             <Route path="/" element={<MainContent />} />
+            <Route path="/product/:id" element={<ProductPage />} />
           </Routes>
         </div>
       </div>
